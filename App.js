3f5181227a148c8ae096ada7bb40aff91b68364e
mydearxym <mydearxym@qq.com>
2017-11-27 15:42:37 +0800

    test

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu. helllllo?</Text>
+        <Text style={styles.text}>Mastani App !!</Text>
       </View>
-    );
+    )
   }
 }
 
@@ -20,4 +18,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  text: {
+    color: 'wheat',
+    fontWeight: 'bold',
+  },
+})
